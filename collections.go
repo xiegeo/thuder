114ff03d8d985507c7178f2e9f591892c59bb233
George Xie <george@xiegeo.com>
2017-03-21 19:20:41 -0700

    work in progess, can compile

--- collections.go
+++ collections.go
@@ -1,14 +1,14 @@
 package thuder
 
 import (
-	"path/filepath"
-	"io"
+	"errors"
 	"os"
+	"path/filepath"
 )
 
 var (
-	ErrBadPath = errors.New("the file path is not of required formate") 
-	ErrNeedDir = errors.New("a directory is required for this operation") 
+	ErrBadPath = errors.New("the file path is not of required formate")
+	ErrNeedDir = errors.New("a directory is required for this operation")
 )
 
 //Node is a node to be modified in the file system, such as files, folders, and
@@ -20,46 +20,47 @@ type Node struct {
 
 //fileContext contains additional node information
 type FileContext struct {
-	from string      //source directory
-	perm os.FileMode //save as mode perm
-	isDelet bool //if true, this file should be removed in a push
+	from    string      //source directory
+	perm    os.FileMode //save as mode perm
+	isDelet bool        //if true, this file should be removed in a push
 }
 
 //NewFileContext Creat a new root node, the fullname must be an absolute path.
-func NewRootNode(fullname string) (*Node, error){
-	if !filepath.IsAbs(fullname){
+func NewRootNode(fullname string) (*Node, error) {
+	if !filepath.IsAbs(fullname) {
 		return nil, ErrBadPath
 	}
-	dir, file := filepath.Split(fullname)
+	dir, _ := filepath.Split(fullname)
 	fc := &FileContext{
-		from: dir
-		perm: os.FileMode(0755)
+		from: dir,
+		perm: os.FileMode(0755),
 	}
 	info, err := os.Stat(fullname)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	return &Node{
-		fc: fc
-		info:info
+		fc:   fc,
+		info: info,
 	}, nil
 }
 
 //Open calls os.Open on the file refrenced by this node
-func (n *Node) Open() (*os.File, error){
+func (n *Node) Open() (*os.File, error) {
 	return os.Open(n.FullName())
 }
 
-func (n *Node) FullName() string{
+func (n *Node) FullName() string {
 	return filepath.Join(n.fc.from, n.info.Name())
 }
 
 //NewFileContext Creat a new child file context to be used by files with the same dir and perm
-func NewFileContext(fi os.FileInfo, parent *Node) *FileContext{
-	
+func NewFileContext(parent *Node) *FileContext {
+	return &FileContext{
+		from: parent.FullName(),
+		perm: parent.fc.perm,
+	}
 }
-
-
 
 //Collection is a document tree that collects meta data of changes in a directory
 //to be made
@@ -67,53 +68,62 @@ type Collection struct {
 	nodes map[string]Node
 }
 
-//Add adds all nodes by filename to the collection, existing node with the same 
+func NewCollection() *Collection {
+	return &Collection{
+		nodes: make(map[string]Node),
+	}
+}
+
+//Add adds all nodes by filename to the collection, existing node with the same
 //name are overwitten.
-func (c *Collection) Add(parent Node) error {
-	if !parent.info.IsDir(){
+func (c *Collection) Add(parent *Node) error {
+	if !parent.info.IsDir() {
 		return ErrNeedDir
 	}
 	f, err := parent.Open()
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	defer f.Close()
 	list, err := f.Readdir(-1)
-	if err != nil{
+	if err != nil {
 		return err
 	}
-	for _, fi := range list{
-		
+	fc := NewFileContext(parent)
+	for _, fi := range list {
+		c.nodes[fi.Name()] = Node{
+			fc:   fc,
+			info: fi,
+		}
 	}
 	return nil
 }
 
 //DirReader can list os.FileInfo, as implemented by os.File
 type DirReader interface {
-	Readdir(n int) ([]FileInfo, error)
+	Readdir(n int) ([]os.FileInfo, error)
 }
-
-
 
 type PullJob struct {
 	Source string //source path
 	Target string //target path
 }
 
+/*
 func (p *PullJob) Do() error {
-	
-	
-	
+
+
+
 	fi, err := os.Stat(dir)
 	if err != nil {
 		return err
 	}
-	
+
 	os.Open(name string)
-	
+
 	c := Collection{}
 	c.Collect()
-	
+
 	return nil
 }
 
@@ -127,3 +137,4 @@ func ChildNodes(dir string, fi os.FileInfo, parent *Node) ([]Node, error) {
 	info fi
 	}
 }
+*/
