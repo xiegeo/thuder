93a0dddac1ecc79948f4e7e265ffa55fc728b45b
George Xie <george@xiegeo.com>
2017-04-29 23:38:19 -0700

    wip: sample app

--- thuder/main.go
+++ thuder/main.go
@@ -5,6 +5,8 @@ sample standalone app
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	logLab "log"
 	"os"
 	"path/filepath"
@@ -21,13 +23,33 @@ func main() {
 	file, err := os.Open(fn)
 	if err != nil {
 		if os.IsNotExist(err) {
-			//todo load default HostConfig
+			//load default HostConfig
+			hc.MediaLocation = mediaLocation()
+			hc.UniqueHostName, err = thuder.GenerateUniqueHostname()
+			if err != nil {
+				panic(err)
+			}
+			hc.Group = groupName()
+			err = saveFile(fn, hc)
+			if err != nil {
+				logE.Println(err)
+			}
 		} else {
 			panic(err)
 		}
+	} else {
+
 	}
 	//todo load HostConfig config file
 	_, _ = hc, file
+}
+
+func saveFile(fn string, v interface{}) error {
+	data, err := json.MarshalIndent(v, "\n", "\t")
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(fn, data, 0644)
 }
 
 //hostConfigPath uses location of executable or current working directory,
