cf8ba13f00fd5aff33d965300eef91743e5b4148
George Xie <george@xiegeo.com>
2017-07-15 17:57:33 +0800

    prepare and test filter

--- filter.go
+++ filter.go
@@ -1,13 +1,14 @@
 package thuder
 
 import (
+	"fmt"
 	"regexp"
 	"time"
 )
 
 type Filter struct {
 	Allow            bool   //allow or block if filter is matched
-	Direction        string `json:",omitempty"` //(pull/push/'empty')
+	Direction        string `json:",omitempty"` //(pull/push/"")
 	FolderOnly       bool   `json:",omitempty"` //matches only folders
 	FileOnly         bool   `json:",omitempty"` //matches only files
 	PathRx           string `json:",omitempty"` //regexp that matches the folder path
@@ -18,6 +19,40 @@ type Filter struct {
 	SubFilters       []Filter `json:",omitempty"` //on match, use subfilters
 }
 
+//Prepare prepares a Filter for usage, and return any errors in filter defination
+func (f *Filter) Prepare() error {
+	if len(f.Direction) > 0 && f.Direction != "pull" && f.Direction != "push" {
+		return fmt.Errorf("unknown Direction %v should be (pull/push/\"\")", f.Direction)
+	}
+	if f.FolderOnly && f.FileOnly {
+		return fmt.Errorf("both FolderOnly and FileOnly")
+	}
+	var err error
+	f.pathRx, err = regexp.Compile(f.PathRx)
+	if err != nil {
+		return fmt.Errorf("PathRx %v", err)
+	}
+	f.nameRx, err = regexp.Compile(f.NameRx)
+	if err != nil {
+		return fmt.Errorf("NameRx %v", err)
+	}
+	if f.LastModifiedDays < 0 {
+		return fmt.Errorf("LastModifiedDays (%v) should be positive", f.LastModifiedDays)
+	}
+	return PrepareFilters(f.SubFilters)
+}
+
+func PrepareFilters(fs []Filter) error {
+	for i := 0; i < len(fs); i++ {
+		err := fs[i].Prepare()
+		if err != nil {
+			return fmt.Errorf("sub(%v) %v", i, err)
+		}
+	}
+	return nil
+}
+
+//Match tests if Node matches filter, and if matched, is it allowed or blocked
 func (f *Filter) Match(n *Node, push bool, now time.Time) (match bool, allow bool) {
 	if len(f.Direction) > 1 {
 		if push && f.Direction != "push" {
--- filter_test.go
+++ filter_test.go
@@ -1,9 +1,125 @@
 package thuder
 
 import (
+	"fmt"
 	"testing"
+	"time"
 )
 
-func TestFilters(t *testing.T) {
+func TestFilter(t *testing.T) {
+	n := &fakeNodes("test", "abc")[0]
+	f := Filter{}
+	now := time.Now()
+	t.Run(fmt.Sprintf("Direction"), func(t *testing.T) {
+		f.Direction = "andsof"
+		expErr(t, f.Prepare())
+		f.Direction = "push"
+		noErr(t, f.Prepare())
+		m, a := f.Match(n, true, now)
+		expect(t, m, a, true, false)
+		f.Allow = true
+		m, a = f.Match(n, true, now)
+		expect(t, m, a, true, true)
+		m, a = f.Match(n, false, now)
+		expect(t, m, a, false, false)
+		f.Direction = "pull"
+		m, a = f.Match(n, false, now)
+		expect(t, m, a, true, true)
+		m, a = f.Match(n, true, now)
+		expect(t, m, a, false, false)
+		f.Direction = ""
+		m, a = f.Match(n, true, now)
+		expect(t, m, a, true, true)
+		m, a = f.Match(n, false, now)
+		expect(t, m, a, true, true)
+	})
+	matches := func(t *testing.T, em bool) {
+		m, a := f.Match(n, false, now)
+		expect(t, m, a, em, em)
+	}
+	t.Run(fmt.Sprintf("FolderOnly"), func(t *testing.T) {
+		matches(t, true)
+		f.FolderOnly = true
+		matches(t, false)
+		n.info.(*testFileInfo).dir = true
+		matches(t, true)
+	})
+	t.Run(fmt.Sprintf("FileOnly"), func(t *testing.T) {
+		f.FileOnly = true
+		expErr(t, f.Prepare())
+		f.FolderOnly = false
+		noErr(t, f.Prepare())
+		matches(t, false)
+		n.info.(*testFileInfo).dir = false
+		matches(t, true)
+	})
+	t.Run(fmt.Sprintf("PathRx"), func(t *testing.T) {
+		f.PathRx = "\\"
+		expErr(t, f.Prepare())
+		f.PathRx = "testtest"
+		noErr(t, f.Prepare())
+		matches(t, false)
+		f.PathRx = "test"
+		noErr(t, f.Prepare())
+		matches(t, true)
+	})
+	t.Run(fmt.Sprintf("NameRx"), func(t *testing.T) {
+		f.NameRx = "\\"
+		expErr(t, f.Prepare())
+		f.NameRx = "x"
+		noErr(t, f.Prepare())
+		matches(t, false)
+		f.NameRx = "a"
+		noErr(t, f.Prepare())
+		matches(t, true)
+	})
+	t.Run(fmt.Sprintf("LastModifiedDays"), func(t *testing.T) {
+		n.info.(*testFileInfo).modtime = now.Add(-30 * time.Hour)
+		f.LastModifiedDays = -1
+		expErr(t, f.Prepare())
+		f.LastModifiedDays = 1
+		noErr(t, f.Prepare())
+		matches(t, false)
+		f.LastModifiedDays = 2
+		noErr(t, f.Prepare())
+		matches(t, true)
+	})
+	t.Run(fmt.Sprintf("SubFilters"), func(t *testing.T) {
+		f2 := f
+		f.SubFilters = []Filter{f2}
+		matches(t, true)
+		f.Allow = false
+		matches(t, true)
+		f.SubFilters[0].LastModifiedDays = -1
+		expErr(t, f.Prepare())
+		f.SubFilters[0].LastModifiedDays = 1
+		noErr(t, f.Prepare())
+		m, a := f.Match(n, false, now)
+		expect(t, m, a, true, false)
+		f.SubFilters = append(f.SubFilters, f2)
+		matches(t, true)
+		f.LastModifiedDays = 1
+		matches(t, false)
+	})
 
+}
+
+func expect(t *testing.T, m, a, em, ea bool) {
+	if m != em || a != ea {
+		t.Errorf("got (%v,%v) expected (%v,%v)", m, a, em, ea)
+	}
+}
+
+func expErr(t *testing.T, err error) {
+	if err == nil {
+		t.Error("error expected")
+	} else {
+		t.Log("expected error: ", err)
+	}
+}
+
+func noErr(t *testing.T, err error) {
+	if err != nil {
+		t.Error(err)
+	}
 }
